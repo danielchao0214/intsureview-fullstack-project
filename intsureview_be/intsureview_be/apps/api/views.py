8935ebec2270967cf4a7ca1276964f2c19375b19
danielchao0214 <34559279+danielchao0214@users.noreply.github.com>
2023-07-04 15:57:15 -0400

    Added colors! And fixed some bugs
    I think everything p much done? Maybe add tests still

--- intsureview_be/intsureview_be/apps/api/views.py
+++ intsureview_be/intsureview_be/apps/api/views.py
@@ -1,24 +1,76 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from rest_framework import permissions
-from intsureview_be.apps.api.serializers import UserSerializer, GroupSerializer
+# from django.contrib.auth.models import User, Group
+# from rest_framework import viewsets
+# from rest_framework import permissions
+# from intsureview_be.apps.api.serializers import UserSerializer, GroupSerializer
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
+from .models import Person
+from .serializers import PersonSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-
-    queryset = User.objects.all().order_by("-date_joined")
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+@api_view(["GET"])
+def get_data(request):
+    people = Person.objects.all()
+    serializer = PersonSerializer(people, many=True)
+    return Response(serializer.data)
 
 
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
+@api_view(["PUT"])
+def add_form(request):
+    # Extract data from request body
+    print("I AM HERE")
+    first_name = request.data.get("first_name")
+    last_name = request.data.get("last_name")
+    color = request.data.get("color")
 
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    if not first_name or not last_name:
+        return Response({"error": "First name and last name are required."}, status=400)
+
+    try:
+        person = Person.objects.get(first_name=first_name, last_name=last_name)
+        # Person with the same first and last name exists, update the color
+        person.color = color
+    except Person.DoesNotExist:
+        # Person does not exist, create a new person
+        person = Person(first_name=first_name, last_name=last_name, color=color)
+
+    # Serialize the updated or new person
+    serializer = PersonSerializer(instance=person, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(
+            serializer.data, status=201
+        )  # Successful creation/update response
+    return Response(serializer.errors, status=400)  # Bad request response
+
+
+@api_view(["DELETE"])
+def delete_person(request, first_name, last_name):
+    try:
+        person = Person.objects.get(first_name=first_name, last_name=last_name)
+        person.delete()
+        return Response(status=204)  # Successful deletion response
+    except Person.DoesNotExist:
+        return Response(status=404)  # Person not found response
+
+
+# class UserViewSet(viewsets.ModelViewSet):
+#     """
+#     API endpoint that allows users to be viewed or edited.
+#     """
+
+#     queryset = User.objects.all().order_by("-date_joined")
+#     serializer_class = UserSerializer
+#     permission_classes = [permissions.IsAuthenticated]
+
+
+# class GroupViewSet(viewsets.ModelViewSet):
+#     """
+#     API endpoint that allows groups to be viewed or edited.
+#     """
+
+#     queryset = Group.objects.all()
+#     serializer_class = GroupSerializer
+#     permission_classes = [permissions.IsAuthenticated]
--- intsureview_be/intsureview_be/urls.py
+++ intsureview_be/intsureview_be/urls.py
@@ -7,15 +7,19 @@ from intsureview_be.apps.api import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.get_data),
+    path("submit/", views.add_form),
+    path("delete/<str:first_name>/<str:last_name>/", views.delete_person),
 ]
 
-router = routers.DefaultRouter()
-router.register(r"users", views.UserViewSet)
-router.register(r"groups", views.GroupViewSet)
+# router = routers.DefaultRouter()
+# router.register(r"users", views.UserViewSet)
+# router.register(r"groups", views.GroupViewSet)
+# router.register(r"items", views.ItemViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
-urlpatterns = [
-    path("", include(router.urls)),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-]
+# # Wire up our API using automatic URL routing.
+# # Additionally, we include login URLs for the browsable API.
+# urlpatterns = [
+#     path("", include(router.urls)),
+#     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+# ]
